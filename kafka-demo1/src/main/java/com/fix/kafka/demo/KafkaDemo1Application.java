13233107cd9bca4c1be9c090bc3d2f232c46fa63
jiang <68jiangxiaogang@163.com>
2019-11-30 14:47:04 +0800

    one

---
+++ kafka-demo1/src/main/java/com/fix/kafka/demo/KafkaDemo1Application.java
@@ -0,0 +1,13 @@
+package com.fix.kafka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDemo1Application.class, args);
+    }
+
+}
